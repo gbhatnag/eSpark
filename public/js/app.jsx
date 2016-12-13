458b0cc9e72b67cb906adad1dd9a9d6e199117f4
Gaurav Cedric Bhatnagar <gaurav.cedric@gmail.com>
2016-12-12 17:50:14 -0800

    Fixing firebase connection; adding content for Test, Ready and Play phases of the UX test

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -5,7 +5,8 @@ var IndexRoute = ReactRouter.IndexRoute;
 var Link = ReactRouter.Link;
 var browserHistory = ReactRouter.browserHistory;
 
-var app = firebase.initializeApp({
+// connect to firebase
+var firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyC1GIOZUV0QF5MgIL_XvqWeMWYp5dFI1Io",
   authDomain: "espark-50d82.firebaseapp.com",
   databaseURL: "https://espark-50d82.firebaseio.com",
@@ -13,14 +14,43 @@ var app = firebase.initializeApp({
   messagingSenderId: "92877010944"
 });
 
+// 'Models' and Forms
+const t = TcombForm;
+const Form = t.form.Form;
+const Methods = t.enums({
+  A: 'A: Reflect with own words',
+  B: 'B: Reflect with given words',
+  C: 'C: No Reflection',
+  R: 'Random choice between A, B, C'
+});
+const TestSchema = t.struct({
+  studentName: t.String,
+  testMethod: Methods
+  /* the following are calculated properties of Test objects
+  starttime  // t.Date
+  endtime    // t.Date
+  actions    // t.list(ActionSchema)
+  score      // t.Number
+  */
+});
+const formOptions = {
+  fields: {
+    testMethod: {
+      nullOption: {value: '-', text: 'Select a variant'}
+    }
+  }
+};
+
+// global state
+var currentTest = null;
+
+// components
 var App = React.createClass({
   render: function () {
     return (
       <div>
         <main className="container-fluid">
-          <p>
-            <Link to="/" className="btn btn-link">Home</Link>
-          </p>
+          <p><Link to="/" className="btn btn-link">Home</Link></p>
           {this.props.children}
         </main>
       </div>
@@ -40,11 +70,31 @@ var Home = React.createClass({
 });
 
 var Test = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  saveTest: function (ev) {
+    ev.preventDefault();
+    const value = this.refs.setupTestForm.getValue();
+    if (!value) return;  // error
+    currentTest = value;
+    var ref = firebase.database().ref('/tests').push();
+    ref.set(currentTest);
+    this.context.router.push('/test/ready');
+  },
+
   render: function () {
     return (
-      <div>
-        <p>Setup the test with this form</p>
-        <Link to="/test/ready" className="btn btn-primary btn-lg">Ready</Link>
+      <div className="jumbotron">
+        <h1>Setup Test</h1>
+        <p>Complete this form and then pass the tablet to the student.</p>
+        <form onSubmit={this.saveTest}>
+          <Form ref="setupTestForm" type={TestSchema} options={formOptions} />
+          <p>
+            <button type="submit" className="btn btn-primary btn-lg">Begin Test</button>
+          </p>
+        </form>
       </div>
     );
   }
@@ -53,9 +103,12 @@ var Test = React.createClass({
 var Ready = React.createClass({
   render: function () {
     return (
-      <div>
-        <p>Present a ready dialog to user (where will the state come from?)</p>
-        <Link to="/test/play" className="btn btn-primary btn-lg">Play</Link>
+      <div className="jumbotron">
+        <h1>Hello, {currentTest.studentName}</h1>
+        <p>Ready?</p>
+        <p>
+          <Link to="/test/play" className="btn btn-primary btn-lg">Watch Video</Link>
+        </p>
       </div>
     );
   }
@@ -64,11 +117,40 @@ var Ready = React.createClass({
 var Play = React.createClass({
   render: function () {
     return (
-      <div>
-        <p>Play the video; next step depends on method a/b/c</p>
-        <Link to="/test/review" className="btn btn-primary btn-lg">Review</Link>
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe id="vid" className="embed-responsive-item"
+          src="https://www.youtube.com/embed/mRdMYuNeAng?enablejsapi=1&amp;rel=0&amp;showinfo=0&amp;autoplay=1"
+          frameBorder="0" allowFullScreen></iframe>
       </div>
     );
+  },
+
+  componentDidMount: function () {
+    var onPlayerReady = function (event) {
+      console.log('player ready');
+    };
+    var onPlayerStateChange = function (event) {
+      var playerStatus = event.data;
+      if (playerStatus == -1) {
+        console.log('unstarted');
+      } else if (playerStatus == 0) {
+        console.log('ended at', player.getCurrentTime());
+      } else if (playerStatus == 1) {
+        console.log('playing at', player.getCurrentTime());
+      } else if (playerStatus == 2) {
+        console.log('paused at', player.getCurrentTime());
+      } else if (playerStatus == 3) {
+        console.log('buffering at', player.getCurrentTime());
+      } else if (playerStatus == 5) {
+        console.log('cued at', player.getCurrentTime());
+      }
+    }
+    var player = new YT.Player('vid', {
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
   }
 });
 
